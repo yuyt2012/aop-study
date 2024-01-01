b1cb3665de96cae08b2a06f60b2fcb78bbdf5c74
yuyt2012 <yuyt2012@gmail.com>
2024-01-01 23:26:10 +0900

    1/1 두번째 커밋

--- src/main/java/hello/aop/order/aop/AspectV2.java
+++ src/main/java/hello/aop/order/aop/AspectV2.java
@@ -4,12 +4,16 @@ import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
 
 @Slf4j
 @Aspect
-public class AspectV1 {
+public class AspectV2 {
+    @Pointcut("execution(* hello.aop.order..*(..))")
+    private void allOrder() {
+    }
 
-    @Around("execution(* hello.aop.order..*(..))")
+    @Around("allOrder()")
     public Object doLog(ProceedingJoinPoint joinPoint) throws Throwable {
         log.info("[log] {}", joinPoint.getSignature());
         return joinPoint.proceed();
